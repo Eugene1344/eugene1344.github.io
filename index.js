7edfd769b5d4ade6873e23dc282276c90311b7ab
Eugene1344 <89064636+Eugene1344@users.noreply.github.com>
2023-10-12 21:23:24 -0500

    Content

---
+++ index.js
@@ -0,0 +1,12 @@
+// const templates = document.querySelectorAll('template');
+//
+// templates.forEach((template) => {
+//     // Clone the content of the template
+//     const clonedContent = template.content.cloneNode(true);
+//
+//     // Find the container where you want to append the cloned content
+//     const componentContent = template.closest('.section').querySelector('.content');
+//
+//     // Append the cloned content to the container
+//     componentContent.appendChild(clonedContent);
+// });
